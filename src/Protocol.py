0eaf5a46d633ec31eac637b50b144a92a3dbe21e
0616039 <ivy0621.cs06@g2.nctu.edu.tw>
2018-10-11 09:54:05 +0000

    merge

--- src/Protocol.py
+++ src/Protocol.py
@@ -7,7 +7,6 @@ Define your own protocol
 '''
 class Protocol(Packet):
     # Set the name of protcol (Task 2.)
-<<<<<<< HEAD
     name = 'Student'
 
     # Define the fields in protocol (Task 2.)
@@ -20,13 +19,8 @@ class Protocol(Packet):
 	}),
 
 	StrField('id', '000000', fmt = 'H', remain = 0),
-=======
     name = ''
 
-    # Define the fields in protocol (Task 2.)
-    fields_desc = [ 
-        
->>>>>>> 0102c067b44a2f7ea2085ada7a98e68292dc765e
     ]
 
 '''
@@ -34,9 +28,5 @@ Add customized protocol into IP layer
 '''
 bind_layers(TCP, Protocol, frag = 0, proto = 99)
 conf.stats_classic_protocols += [Protocol]
-<<<<<<< HEAD
 conf.stats_dot11_protocols += [Protocol]
 
-=======
-conf.stats_dot11_protocols += [Protocol]
->>>>>>> 0102c067b44a2f7ea2085ada7a98e68292dc765e
